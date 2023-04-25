6bf5f9968c978c625bcd51ac13a73166f867f802
Haruto.S <c0a220828d@edu.teu.ac.jp>
2023-04-25 14:21:05 +0900

    example02

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,14 +1,18 @@
 import pygame as pg
 import sys
-
+import random
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")
+    bg_img = pg.image.load("2day/ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("2day/ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    x,y=random.randint(0,1000),random.randint(0,900) 
 
     while True:
         for event in pg.event.get():
@@ -18,6 +22,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [x, y])
 
         pg.display.update()
         clock.tick(1000)
